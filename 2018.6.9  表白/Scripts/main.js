e433fa5fe6d9f7bc601c0d16484006e49d31e521
waggag <waggag@outlook.com>
2019-05-22 16:29:35 +0800

    后台登陆界面

--- 2018.6.9  表白/Scripts/main.js
+++ 2018.6.9  表白/Scripts/main.js
@@ -2,16 +2,15 @@ var bgs = document.querySelectorAll('.bg');
 
 for (var i = 0; i < bgs.length; i++) {
 	var url = bgs[i].getAttribute("data-url");
-	bgs[i].style.backgroundImage = "url("+ url +")";
+	bgs[i].style.backgroundImage = "url(" + url + ")";
 };
-
 //ScrollTop
-	$(".up").click(function() {
-		$(".stage").animate({
-			scrollTop: 0
-		}, {
-			duration: 500,
-			easing: "swing"
-		});
-		return false;
+$(".up").click(function() {
+	$(".stage").animate({
+		scrollTop: 0
+	}, {
+		duration: 500,
+		easing: "swing"
 	});
+	return false;
+});
--- 2018.6.9  表白/Scripts/time.js
+++ 2018.6.9  表白/Scripts/time.js
@@ -1,19 +1,15 @@
-		
-		var together = new Date();
-		
+var together = new Date();
 		//最后一页的时间 年,月,
-		together.setFullYear(2016,9,1);
-		
+		together.setFullYear(2019, 2, 24);
+
 		//setHours() 方法用于设置指定的时间的小时,分钟,秒,毫秒字段
 		together.setHours(0);
 		together.setMinutes(0);
 		together.setSeconds(0);
 		together.setMilliseconds(0);
-		
+
 		//setInterval()方法可按照指定的周期来调用函数或计算表达式。
 		timeElapse(together);
-			setInterval(function () {
-				timeElapse(together);
-			}, 1000);
-			
-			
+		setInterval(function() {
+			timeElapse(together);
+		}, 1000);
