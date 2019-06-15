76ba72f79c34a92bcc48d721178daed32dc6394b
waggag <waggag@outlook.com>
2019-06-15 22:24:43 +0800

    2019.6.15

--- 2018.6.9  翡翠杯/js/index.js
+++ 2018.6.9  翡翠杯/js/index.js
@@ -1,4 +1,4 @@
-window.onload = function(){
+window.onload = function () {
     //获取元素
     var banner = document.getElementsByClassName("banner")[0];
     var leftBtn = document.getElementsByClassName("left_btn")[0];
@@ -6,32 +6,32 @@ window.onload = function(){
 
     var num = 0;
     //定义一个数组，里面存放所有需要切换的图片
-    var bannerPics = ["img/lunbo1.jpg","img/lunbo2.jpg","img/lunbo4.jpg","img/lunbo5.jpg"];
+    var bannerPics = ["img/lunbo1.jpg", "img/lunbo2.jpg", "img/lunbo4.jpg", "img/lunbo5.jpg"];
 
-    banner.style.backgroundImage = "url("+bannerPics[num]+")";
+    banner.style.backgroundImage = "url(" + bannerPics[num] + ")";
 
     // 事件单击事件
-    leftBtn.onclick = function(){
+    leftBtn.onclick = function () {
         // alert("左左左");
         // console.log(banner.style.backgroundTmage);
 
         //拿到数组里面的图片地址
         // banner.style.backgroundTmage = "url(img/lunbo3.jpg)"
         num++;
-        if(num > bannerPics.length - 1){
+        if (num > bannerPics.length - 1) {
             num = 0;
         }
-        banner.style.backgroundImage = "url("+bannerPics[num]+")";
+        banner.style.backgroundImage = "url(" + bannerPics[num] + ")";
     }
 
-    rightBtn.onclick=function(){
+    rightBtn.onclick = function () {
         // alert("右右右");
         num--;
-        if(num < 0){
-            num = bannerPics.length-1;
+        if (num < 0) {
+            num = bannerPics.length - 1;
         }
 
-        banner.style.backgroundImage = "url("+bannerPics[num]+")";
+        banner.style.backgroundImage = "url(" + bannerPics[num] + ")";
     }
 
 }
